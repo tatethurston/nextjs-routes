29011be87d13c1d13df662538e2f011e36528aa4
Sachin Raja <sachinraja2349@gmail.com>
2022-07-08 11:13:03 -0700

    remove prettier (#26)
    * remove prettier
    
    * use double quotes

--- src/cli.ts
+++ src/cli.ts
@@ -6,20 +6,12 @@ import { getPagesDirectory, findFiles, generate, nextRoutes } from "./utils.js";
 
 const NEXTJS_PAGES_DIRECTORY = join(".", getPagesDirectory());
 
-async function main(): Promise<void> {
+function main(): void {
   const files = findFiles(NEXTJS_PAGES_DIRECTORY);
   const routes = nextRoutes(files);
-  let generated = generate(routes);
-
-  try {
-    const prettier = await import("prettier");
-    generated = prettier.format(generated, { parser: "typescript" });
-    // eslint-disable-next-line no-empty
-  } catch (e) {
-    console.warn(e);
-  }
+  const generated = generate(routes);
 
   writeFileSync("nextjs-routes.d.ts", generated);
 }
 
-void main();
+main();

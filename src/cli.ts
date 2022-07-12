8e7753599f1aa1c88ed37b8e8e4a9b7a37512c1b
Tate Thurston <tatethurston@gmail.com>
2022-07-12 12:04:15 -0700

    refactor: extract logic from cli into a function

--- src/cli.ts
+++ src/cli.ts
@@ -1,26 +1,5 @@
 #!/usr/bin/env node
 
-import { writeFileSync } from "fs";
-import { join } from "path";
-import { getPagesDirectory, findFiles, generate, nextRoutes } from "./utils.js";
+import { cli } from "./core.js";
 
-const logger: Pick<Console, "error"> = {
-  error: (str: string) => console.error("[nextjs-routes] " + str),
-};
-
-const pagesDirectory = getPagesDirectory();
-// istanbul ignore else: covered by e2e.test
-if (!pagesDirectory) {
-  logger.error(`Could not find a Next.js pages directory. Expected to find either pages(1) or src/pages(2).
-
-1. https://nextjs.org/docs/basic-features/pages
-2. https://nextjs.org/docs/advanced-features/src-directory
-`);
-  process.exit(1);
-} else {
-  const files = findFiles(join(".", pagesDirectory));
-  const routes = nextRoutes(files, pagesDirectory);
-  const generated = generate(routes);
-
-  writeFileSync("nextjs-routes.d.ts", generated);
-}
+cli();

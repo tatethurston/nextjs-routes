ec35a1bc48f8962d0a3fe262f175c49fce0dc3f6
Alex Gorbatchev <alexgorbatchev@users.noreply.github.com>
2022-11-02 04:04:59 +0000

    Adds `cwd` to help with monorepos

--- src/utils.ts
+++ src/utils.ts
@@ -2,17 +2,17 @@ import { existsSync, readdirSync, statSync } from "fs";
 import { join } from "path";
 
 // istanbul ignore next: io mocking not worthwhile
-export function getPagesDirectory(): string | undefined {
+export function getPagesDirectory(cwd = process.cwd()): string | undefined {
   const dirs = ["pages", join("src", "pages")];
   for (const dir of dirs) {
-    if (existsSync(dir)) {
+    if (existsSync(join(cwd, dir))) {
       return dir;
     }
   }
 }
 
-export function getAppDirectory(): string | undefined {
-  if (existsSync("app")) {
+export function getAppDirectory(cwd = process.cwd()): string | undefined {
+  if (existsSync(join(cwd, "app"))) {
     return "app";
   }
 }

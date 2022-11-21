8989bda6749edd447a8bc3c10cd60eac84a6a26e
Tate Thurston <tatethurston@gmail.com>
2022-11-20 17:25:52 -0800

    Link anchor props

--- src/core.ts
+++ src/core.ts
@@ -128,14 +128,20 @@ declare module "nextjs-routes" {
 declare module "next/link" {
   import type { Route } from "nextjs-routes";
   import type { LinkProps as NextLinkProps } from "next/dist/client/link";
-  import type { PropsWithChildren, MouseEventHandler } from "react";
+  import type {
+    AnchorHTMLAttributes,
+    DetailedReactHTMLElement,
+    PropsWithChildren,
+    MouseEventHandler,
+  } from "react";
   export * from "next/dist/client/link";
 
   type Query = { query?: { [key: string]: string | string[] | undefined } };
   type StaticRoute = Exclude<Route, { query: any }>["pathname"];
 
   export interface LinkProps<Href extends Route | Query = Route | Query>
-    extends Omit<NextLinkProps, "href" | "locale"> {
+    extends Omit<NextLinkProps, "href" | "locale">,
+      AnchorHTMLAttributes<HTMLAnchorElement> {
     href: Href;
     locale?: ${!i18n.locales.length ? "false" : `Locale | false`};
   }
@@ -175,7 +181,7 @@ declare module "next/router" {
 
   interface TransitionOptions extends Omit<NextTransitionOptions, "locale"> {
     locale?: ${!i18n.locales.length ? "false" : `Locale | false`};
-  };
+  }
 
   export type NextRouter<P extends Route["pathname"] = Route["pathname"]> =
     Extract<Route, { pathname: P }> &
@@ -229,7 +235,7 @@ declare module "next/router" {
           options?: TransitionOptions
         ): Promise<boolean>;
         route: P;
-      }
+      };
 
   export function useRouter<P extends Route["pathname"]>(): NextRouter<P>;
 }

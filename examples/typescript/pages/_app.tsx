a0b1f3502a0a5aff3d939ebab25341d3e03fc2dd
Tate Thurston <tatethurston@gmail.com>
2023-01-20 11:57:03 -0800

    v1.0.8 (#120)

--- examples/typescript/pages/_app.tsx
+++ examples/typescript/pages/_app.tsx
@@ -1,6 +1,4 @@
 import type { AppProps } from "next/app";
-import withRoutes from "nextjs-routes/config";
-withRoutes({});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

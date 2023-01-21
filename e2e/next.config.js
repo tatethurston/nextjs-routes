6a483cb2a7b532f1bca936c5e54ed8beba7863ff
ckt <65409152+cktsun1031@users.noreply.github.com>
2023-01-21 04:25:22 +0000

    fix plugin importing

--- e2e/next.config.js
+++ e2e/next.config.js
@@ -1,8 +1,8 @@
 const {withRoutes} = require("nextjs-routes/config");
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   typescript: {
     ignoreBuildErrors: true,
   },
